508474a43a3de6d32797d44d05ad1b7f375269db
zNitche <53448802+zNitche@users.noreply.github.com>
2024-04-21 20:23:10 +0200

    implementation of serving react frontend

--- routes/core.py
+++ routes/core.py
@@ -1,9 +1,30 @@
-from lightberry import Router, Response
+from lightberry import Router, Response, FileResponse
+from lightberry.utils import files_utils
 
 
 core = Router("core")
 
 
+@core.route("/assets/:asset_name")
+async def serve_assets(request, asset_name):
+    serialized_path = f"/files/assets/{asset_name.replace('..', '')}"
+
+    if not files_utils.file_exists(serialized_path):
+        return Response(status_code=404)
+
+    response = FileResponse(file_path=serialized_path)
+
+    if serialized_path.endswith(".gz"):
+        response.add_header("Content-Encoding", "gzip")
+
+    if serialized_path.endswith(".js.gz"):
+        response.add_header("Content-Type", "text/javascript")
+    elif serialized_path.endswith(".css.gz"):
+        response.add_header("Content-Type", "text/css")
+
+    return response
+
+
 @core.catch_all()
-async def home(request):
-    return Response(200, payload="Home")
+async def web_panel(request):
+    return FileResponse(file_path="/files/index.html")
