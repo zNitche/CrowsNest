00613a3b1b32d1c927d46c25bb914ee0d3e32542
zNitche <53448802+zNitche@users.noreply.github.com>
2024-04-24 21:17:10 +0200

    endpoint for getting services + sorting and searching

--- crows_nest/routes/api.py
+++ crows_nest/routes/api.py
@@ -1,4 +1,5 @@
-from lightberry import Router, Response
+from lightberry import Router, Response, shortcuts
+from crows_nest import db
 
 
 api = Router("api", url_prefix="/api")
@@ -7,3 +8,28 @@ api = Router("api", url_prefix="/api")
 @api.route("/healthcheck")
 async def healthcheck(request):
     return Response(payload="ok")
+
+
+@api.route("/services")
+async def get_services(request):
+    search_name_param = request.query_params.get("name")
+    sort_param = request.query_params.get("sort")
+    sort_param = sort_param if sort_param in ["asc", "desc"] else "desc"
+
+    services = db.read_from("services")
+
+    services = services if services else []
+    services.sort(reverse=True if sort_param == "desc" else False,
+                  key=lambda e: e["addedAt"])
+
+    filtered_services = []
+
+    if search_name_param:
+        for service in services:
+            if service["name"].startswith(search_name_param):
+                filtered_services.append(service)
+
+    else:
+        filtered_services = services
+
+    return Response(payload=shortcuts.jsonify(filtered_services))
