522136caea3d474222cfb4335898375f54cda32d
zNitche <53448802+zNitche@users.noreply.github.com>
2024-04-24 21:17:44 +0200

    app after request handler for proper cors headers setup

--- crows_nest/routes/__init__.py
+++ crows_nest/routes/__init__.py
@@ -1,2 +1,16 @@
 from crows_nest.routes.core import core
 from crows_nest.routes.api import api
+
+from lightberry import AppContext
+
+current_app = AppContext.get_current_app()
+
+
+@current_app.after_request()
+async def after_request(response):
+    # meant to be LAN only so we don't have to care about strict cors
+    response.add_header("Access-Control-Allow-Origin", "*")
+    response.add_header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+    response.add_header("Access-Control-Allow-Headers", "Content-Type")
+
+    return response
