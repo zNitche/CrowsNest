37698efab5dbf9f21482467205bd1b2e0d23c6be
zNitche <53448802+zNitche@users.noreply.github.com>
2024-04-25 18:40:09 +0200

    separated database module

--- crows_nest/routes/api.py
+++ crows_nest/routes/api.py
@@ -1,6 +1,7 @@
-from lightberry import Router, Response, shortcuts
-from crows_nest import db
-
+from lightberry import Router, Response
+from lightberry.shortcuts import jsonify
+from crows_nest import db, services_availability_cache
+from crows_nest.models import ServiceModel
 
 api = Router("api", url_prefix="/api")
 
@@ -10,26 +11,33 @@ async def healthcheck(request):
     return Response(payload="ok")
 
 
+@api.route("/services/:id/availability")
+async def check_service_availability(request, id):
+    service = db.query(ServiceModel, search_by={"id": int(id)}, first=True)
+
+    if service is None:
+        return Response(status_code=404)
+
+    is_available = services_availability_cache.read(id)
+
+    return Response(payload=jsonify({"is_available": is_available if is_available else False}))
+
+
 @api.route("/services")
 async def get_services(request):
     search_name_param = request.query_params.get("name")
     sort_param = request.query_params.get("sort")
-    sort_param = sort_param if sort_param in ["asc", "desc"] else "desc"
 
-    services = db.read_from("services")
+    services = db.query(ServiceModel,
+                        sort_by={
+                            "key": "added_at",
+                            "type": sort_param
+                        },
+                        search_by={
+                            "name": search_name_param
+                        }
+                        )
 
-    services = services if services else []
-    services.sort(reverse=True if sort_param == "desc" else False,
-                  key=lambda e: e["addedAt"])
+    services = [service.dump() for service in services]
 
-    filtered_services = []
-
-    if search_name_param:
-        for service in services:
-            if service["name"].startswith(search_name_param):
-                filtered_services.append(service)
-
-    else:
-        filtered_services = services
-
-    return Response(payload=shortcuts.jsonify(filtered_services))
+    return Response(payload=jsonify(services))
---
+++ crows_nest/storage/database.py
@@ -0,0 +1,58 @@
+import json
+from lightberry.utils import files_utils
+
+
+class Database:
+    def __init__(self):
+        self.db_path = None
+
+    def __read_from(self, section=None, raise_exception=True):
+        if raise_exception:
+            if not self.db_path or not files_utils.file_exists(self.db_path):
+                raise Exception("db file is unavailable")
+
+        with open(self.db_path, "r") as db_file:
+            content = json.loads(db_file.read())
+
+        return content.get(section) if section else content
+
+    def __search_data(self, raw_data, search_by):
+        filtered_data = []
+
+        if search_by:
+            for item in raw_data:
+                is_matching = True
+
+                for key, value in search_by.items():
+                    if key and value is not None:
+                        item_key_val = item.get(key)
+                        check = (value in item_key_val) if isinstance(value, str) else (value == item_key_val)
+
+                        if item_key_val is None or not check:
+                            is_matching = False
+
+                if is_matching:
+                    filtered_data.append(item)
+
+        else:
+            filtered_data = raw_data
+
+        return filtered_data
+
+    def query(self, model, search_by=None, sort_by=None, first=False):
+        data = self.__read_from(section=model.section_name())
+        filtered_data = self.__search_data(data, search_by)
+
+        if sort_by:
+            sort_by_key = sort_by["key"]
+            sort_by_type = sort_by.get("type") if sort_by["type"] in ["asc", "desc"] else "desc"
+
+            filtered_data.sort(reverse=True if sort_by_type == "desc" else False,
+                               key=lambda e: e[sort_by_key])
+
+        models = [model(data=item) for item in filtered_data]
+
+        if first:
+            models = models[0] if len(models) > 0 else None
+
+        return models
