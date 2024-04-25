e765e163d14b7242e5f2d006b8083aa77777365c
zNitche <53448802+zNitche@users.noreply.github.com>
2024-04-25 18:38:48 +0200

    script for minifying database

---
+++ .tools/convert_db.py
@@ -0,0 +1,34 @@
+import json
+import os
+import argparse
+
+
+def main(args):
+    db_path = args.db_path
+    pack_db = args.minify
+
+    if db_path and os.path.exists(db_path):
+        with open(db_path, "r") as db_file:
+            content = json.loads(db_file.read())
+
+        with open(db_path, "w") as db_file:
+            db_file.write(json.dumps(content,
+                                     indent=None if pack_db else 4,
+                                     separators=(',', ':')))
+
+    else:
+        print("db file doesn't exist")
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--db_path", type=str, default=None, help="database path", required=True)
+    parser.add_argument("--minify", action=argparse.BooleanOptionalAction,
+                        type=bool, default=False, help="minify db file", required=False)
+    opt = parser.parse_args()
+
+    return opt
+
+
+if __name__ == '__main__':
+    main(get_args())
--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # CrowsNest
 lightberryAPI + Raspberry Pi Pico W = LAN services hub
 
-Project utilize `lightberryAPI` as part of MicroPython firmware
+- Project utilize `lightberryAPI` as part of MicroPython firmware
 [see](https://github.com/zNitche/lightberryAPI?tab=readme-ov-file#as-a-micropython-frozen-module)
+
+- database tools
+  - minify `db.json`
+  ```
+  python3 .tools/convert_db.py --db_path ./db.json --minify
+  ```
+  - de-minify `db.json`
+  ```
+  python3 .tools/convert_db.py --db_path ./db.json
+  ```
