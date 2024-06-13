49602bdd4603ad05dbfac4386ceb0990898323ea
zNitche <53448802+zNitche@users.noreply.github.com>
2024-06-13 18:24:10 +0200

    replaced blocking requests with async ones using microHTTP

--- crows_nest/tasks/check_service_availability_task.py
+++ crows_nest/tasks/check_service_availability_task.py
@@ -1,21 +1,25 @@
 from lightberry import ATaskBase
 from crows_nest import db, services_availability_cache
 from crows_nest.models import ServiceModel
-import requests
+from microHTTP import MicroHttpClient
 
 
 class CheckServiceAvailabilityTask(ATaskBase):
     def __init__(self, logging=False):
-        super().__init__(periodic_interval=30, logging=logging)
+        super().__init__(periodic_interval=15, logging=logging)
+
+        self.http_client = MicroHttpClient()
 
     async def task(self):
         services: list[ServiceModel] = db.query(ServiceModel)
 
         for service in services:  # type: ServiceModel
-            target_url = f"{service.url}:{service.port}{service.health_check_endpoint}"
-
             try:
-                res = requests.get(url=target_url, timeout=3)
+                res = await self.http_client.get(host=service.url,
+                                                 url=service.health_check_endpoint,
+                                                 port=int(service.port),
+                                                 timeout=3)
+
                 is_available = True if res.status_code == 200 else False
 
             except Exception:
